b6076245a4c20be2ba1d66efb7ab40750373a3dc
Ahmed Anwar Gad <ahmed.anwar2003@gmail.com>
2024-09-18 22:05:45 +0300

    Add files via upload

--- src/nezumi_2.1.ino
+++ src/nezumi_2.1.ino
@@ -1,14 +1,36 @@
-#include "../lib/motor_control.h"
-#include "../lib/maze.h"
+#include "motor_control.h"
+#include "maze.h"
+#include "constants.h"
+
+void leftEncoderISR() {
+  // Determine direction based on the state of the second hall sensor (B)
+  if (digitalRead(LEFT_ENCODER_PIN_A) == digitalRead(LEFT_ENCODER_PIN_B)) {
+    left_ticks++;  // Moving forward
+  } else {
+    left_ticks--;  // Moving backward
+  }
+}
+
+void rightEncoderISR() {
+  // Determine direction based on the state of the second hall sensor (B)
+  if (digitalRead(RIGHT_ENCODER_PIN_A) == digitalRead(RIGHT_ENCODER_PIN_B)) {
+    right_ticks++;  // Moving forward
+  } else {
+    right_ticks--;  // Moving backward
+  }
+}
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
 
-  pinMode(LEFT_ENCODER_PIN, INPUT_PULLUP);
-  pinMode(RIGHT_ENCODER_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_PIN), leftEncoderISR, RISING);
-  attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER_PIN), rightEncoderISR, RISING);
+  pinMode(LEFT_ENCODER_PIN_A, INPUT_PULLUP);
+  pinMode(LEFT_ENCODER_PIN_B, INPUT_PULLUP);
+  pinMode(RIGHT_ENCODER_PIN_A, INPUT_PULLUP);
+  pinMode(RIGHT_ENCODER_PIN_B, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_PIN_A), leftEncoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER_PIN_A), rightEncoderISR, CHANGE);
 
   pinMode(LEFT_MOTOR_PIN1, OUTPUT);
   pinMode(LEFT_MOTOR_PIN2, OUTPUT);
@@ -17,17 +39,13 @@ void setup() {
 
   initializeFloodFill();
 }
-void leftEncoderISR() {
-  left_ticks++;
-}
-
-void rightEncoderISR() {
-  right_ticks++;
-}
 
 void loop() {
+  // Update the maze walls after every movement and sensor read
+  updateMazeWalls();
+  
   updateFloodFill();
   chooseNextMove();
-  moveRobot();
+  moveRobot(direction);  // Move the robot based on chosen direction
   delay(500);
 }
